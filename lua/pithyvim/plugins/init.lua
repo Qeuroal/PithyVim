ff06223e7429123a679f6a71bebbedde52b7d707
Qeuroal <12997837+qeuroal@user.noreply.gitee.com>
2024-11-19 17:01:02 +0800

    plugin: tmux

--- lua/pithyvim/plugins/init.lua
+++ lua/pithyvim/plugins/init.lua
@@ -37,10 +37,12 @@ return {
         terminal = {
           win = {
             keys = {
-              nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
-              nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
-              nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
-              nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
+              -- deprecated: use nvim-tmux-navigation
+              --
+              -- nav_h = { "<C-h>", term_nav("h"), desc = "Go to Left Window", expr = true, mode = "t" },
+              -- nav_j = { "<C-j>", term_nav("j"), desc = "Go to Lower Window", expr = true, mode = "t" },
+              -- nav_k = { "<C-k>", term_nav("k"), desc = "Go to Upper Window", expr = true, mode = "t" },
+              -- nav_l = { "<C-l>", term_nav("l"), desc = "Go to Right Window", expr = true, mode = "t" },
             },
           },
         },
