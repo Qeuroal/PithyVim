89216f6b9a97202f066f886f109b0bddeab28697
Qeuroal <Qeuroal@163.com>
2024-12-14 15:10:21 +0800

    keymap: avoid c-u, c-d

--- lua/pithyvim/config/keymaps.lua
+++ lua/pithyvim/config/keymaps.lua
@@ -9,8 +9,8 @@ map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true
 map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
 map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-map({ "n", "x" }, "gj", "'j'", { desc = "Down", expr = true, silent = true, noremap = false })
-map({ "n", "x" }, "gk", "'k'", { desc = "Up", expr = true, silent = true, noremap = false })
+-- map({ "n", "x" }, "gj", "'j'", { desc = "Down", expr = true, silent = true, noremap = false })
+-- map({ "n", "x" }, "gk", "'k'", { desc = "Up", expr = true, silent = true, noremap = false })
 
 
 -- -- deprecated: use nvim-tmux-navigation
