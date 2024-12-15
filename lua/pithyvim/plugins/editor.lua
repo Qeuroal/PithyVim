02dda2a877d3149e744e6794da6d839860a04021
Qeuroal <Qeuroal@163.com>
2024-12-15 19:13:55 +0800

    editor

--- lua/pithyvim/plugins/editor.lua
+++ lua/pithyvim/plugins/editor.lua
@@ -185,8 +185,8 @@ return {
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-      -- { "<leader>j", mode = { "n", "x", "o" }, function () require("flash").jump({ search = { mode = "search", max_length = 0 }, label = { after = { 0, 0 } }, pattern = "^" }) end, desc = "Flash jump a line" },
-      -- { "<leader>k", mode = { "n", "x", "o" }, function () require("flash").jump({continue = true}) end, desc = "Flash jump a line" },
+      { "<leader>j", mode = { "n", "x", "o" }, function () require("flash").jump({ search = { mode = "search", max_length = 0 }, label = { after = { 0, 0 } }, pattern = "^" }) end, desc = "Flash jump a line" },
+      { "<leader>J", mode = { "n", "x", "o" }, function () require("flash").jump({continue = true}) end, desc = "Flash jump a line" },
     },
   },
 
