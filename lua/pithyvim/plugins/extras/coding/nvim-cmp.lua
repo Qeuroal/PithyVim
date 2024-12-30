8e340f0e8899023a5d53a3d01860ba3a9bea82f8
Qeuroal <Qeuroal@163.com>
2024-12-30 16:44:33 +0800

    nvim-cmp

--- lua/pithyvim/plugins/extras/coding/nvim-cmp.lua
+++ lua/pithyvim/plugins/extras/coding/nvim-cmp.lua
@@ -38,9 +38,13 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-Space>"] = cmp.mapping.complete(),
+          --{{{> Qeuroal
+          ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-n>"] = cmp.mapping.abort(),
+          ["<C-p>"] = cmp.mapping.abort(),
+          ["<C-o>"] = cmp.mapping.complete(),
+          --<}}}
           ["<CR>"] = PithyVim.cmp.confirm({ select = auto_select }),
           ["<C-y>"] = PithyVim.cmp.confirm({ select = true }),
           ["<S-CR>"] = PithyVim.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
