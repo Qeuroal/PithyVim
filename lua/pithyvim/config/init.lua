43519280ad74c86d1140d8231d0e323853c5e556
Qeuroal <Qeuroal@163.com>
2025-02-14 18:37:04 +0800

    snack_explore -> neo-tree

--- lua/pithyvim/config/init.lua
+++ lua/pithyvim/config/init.lua
@@ -375,6 +375,10 @@ function M.get_defaults()
       { name = "nvim-cmp", extra = "coding.nvim-cmp" },
     },
     explorer = {
+      --{{{> Qeuroal
+      -- snacks_explorer has question that keymap <c-j> isn't switch windows and grep error
+      { name = "neo-tree", extra = "editor.neo-tree" },
+      --<}}}
       { name = "snacks", extra = "editor.snacks_explorer" },
       { name = "neo-tree", extra = "editor.neo-tree" },
     },
