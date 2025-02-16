5f1739bdc1ea848f267826e18613af776f3a8e67
Qeuroal <Qeuroal@163.com>
2025-02-16 11:59:09 +0800

    keymap

--- lua/pithyvim/config/keymaps.lua
+++ lua/pithyvim/config/keymaps.lua
@@ -242,6 +242,6 @@ if vim.fn.has("nvim-0.11") == 0 then
 end
 
 -- fold
-map("n", "<leader>uzt", "<cmd>lua PithyVim.ui.toggleFoldmethod()<cr>", { desc = "Toggle between 'expr' and 'marker' for foldmethod settings." })
+map("n", "<leader>uz", "<cmd>lua PithyVim.ui.toggleFoldmethod()<cr>", { desc = "Toggle between 'expr' and 'marker' for foldmethod settings." })
 
 
