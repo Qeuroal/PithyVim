758e0185eb2a6f8aed3914cf250e77e8e3f43c8f
Qeuroal <Qeuroal@163.com>
2025-02-23 08:53:40 +0800

    merge update

--- lua/pithyvim/config/keymaps.lua
+++ lua/pithyvim/config/keymaps.lua
@@ -194,7 +194,7 @@ map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 
 -- highlights under cursor
 map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
-map("n", "<leader>uI", "<cmd>InspectTree<cr>", { desc = "Inspect Tree" })
+map("n", "<leader>uI", function() vim.treesitter.inspect_tree() vim.api.nvim_input("I") end, { desc = "Inspect Tree" })
 
 -- PithyVim Changelog
 map("n", "<leader>L", function() PithyVim.news.changelog() end, { desc = "PithyVim Changelog" })
