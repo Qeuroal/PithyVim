76a3565d7f09f008f91dff9f1c5923379110269a
Qeuroal <Qeuroal@163.com>
2025-05-11 14:52:09 +0800

    fix: mason

--- lua/pithyvim/plugins/extras/formatting/prettier.lua
+++ lua/pithyvim/plugins/extras/formatting/prettier.lua
@@ -58,7 +58,7 @@ M.has_parser = PithyVim.memoize(M.has_parser)
 
 return {
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     opts = { ensure_installed = { "prettier" } },
   },
 
