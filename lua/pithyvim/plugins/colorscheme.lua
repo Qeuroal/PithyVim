233a3365e244c69d16512028bdf37430963aab07
Qeuroal <Qeuroal@163.com>
2025-05-12 08:09:42 +0800

    feat: add everforest colorscheme plugin

--- lua/pithyvim/plugins/colorscheme.lua
+++ lua/pithyvim/plugins/colorscheme.lua
@@ -16,6 +16,20 @@ return {
     opts = { style = "moon" },
   },
 
+  -- everforest
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        -- Your config here
+      })
+    end,
+  },
+
   -- catppuccin
   {
     "catppuccin/nvim",
