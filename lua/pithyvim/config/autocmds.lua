c3bc733f0ff26f9a9880384483a85ac1d08c3516
Qeuroal <Qeuroal@163.com>
2025-09-14 14:24:42 +0800

    fix: 将 Markdown 缩进宽度从 2 改为 3

--- lua/pithyvim/config/autocmds.lua
+++ lua/pithyvim/config/autocmds.lua
@@ -133,9 +133,9 @@ vim.api.nvim_create_autocmd("FileType", {
   group = augroup("markdown_auto_indent"),
   pattern = "markdown",
   callback = function()
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 3
+    vim.opt_local.tabstop = 3
+    vim.opt_local.softtabstop = 3
   end,
 })
 --<}}}
