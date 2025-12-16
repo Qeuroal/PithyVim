82016ea3e17a8fa0955a50322392e98811dc547e
Qeuroal <Qeuroal@163.com>
2025-12-16 13:11:54 +0800

    feat: 添加用户自定义默认代码片段加载路径

--- lua/pithyvim/plugins/extras/coding/luasnip.lua
+++ lua/pithyvim/plugins/extras/coding/luasnip.lua
@@ -18,6 +18,8 @@ return {
           -- WARNING: 如果要使用自定义片段, 必须打开 luasnip 插件
           require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets/from_vscode" } })
           require("luasnip.loaders.from_snipmate").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets/from_snipmate" } })
+          -- Load user custom default snippets
+          require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
           -- require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
           --<}}}
         end,
