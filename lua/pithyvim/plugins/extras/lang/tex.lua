6a759bbc18a451c1ef7eedd171f23dc387aca50e
Qeuroal <Qeuroal@163.com>
2026-02-03 12:12:14 +0800

    feat: 调整 VimTeX 配置，优化补全与语法隐藏

--- lua/pithyvim/plugins/extras/lang/tex.lua
+++ lua/pithyvim/plugins/extras/lang/tex.lua
@@ -28,30 +28,48 @@ return {
     --{{{> Qeuroal
     -- ft = { "tex", "plaintex", "bib", "sty" },
     init = function ()
-      vim.g.vimtex_quickfix_mode = 0
-      vim.g.tex_conceal='abdmg'
-      vim.g.tex_flavor='latex'
+      vim.g.vimtex_quickfix_mode = 0 -- 编译出错时自动打开 quickfix, 0, 从不; 1, 出错时; 2, 总是
+      vim.g.tex_conceal = 'abdmg'
+      vim.g.tex_flavor = 'latex'
       vim.g.vimtex_view_method = "skim"
-      vim.g.vimtex_view_skim_sync=1
-      vim.g.vimtex_view_skim_activate=0
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 0
+      vim.g.vimtex_view_automatic = 0
+      vim.g.vimtex_complete_enabled = 0 -- 关闭内建补全
+      vim.g.vimtex_imaps_enabled = 0 -- 禁用 VimTeX 的 insert-mode mappings, 防止与 UltiSnips / LuaSnip 冲突
       vim.g.vimtex_syntax_conceal = {
         accents = 1,
         ligatures = 1,
         cites = 1,
         fancy = 1,
-        spacing = 0,
+        spacing = 0, -- default 1
         greek = 1,
-        math_bounds = 0,
+        math_bounds = 0, -- default 1
         math_delimiters = 1,
         math_fracs = 1,
         math_super_sub = 1,
         math_symbols = 1,
-        sections = 1,
+        sections = 1, -- default 0
         styles = 1,
       }
-      -- default = {'math_super_sub': 1, 'accents': 1, 'greek': 1, 'styles': 1, 'math_fracs': 1,
-      --            'math_symbols': 1, 'spacing': 1, 'ligatures': 1 , 'fancy': 1, 'sections': 0,
-      --            'math_delimiters': 1, 'math_bounds': 1, 'cites': 1}
+      vim.g.vimtex_quickfix_ignore_filters = {
+        -- "Command terminated with space",
+        -- "LaTeX Font Warning: Font shape",
+        -- "Package caption Warning: The option",
+        -- [[Underfull \\hbox (badness [0-9]*) in]],
+        -- "Package enumitem Warning: Negative labelwidth",
+        -- [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in]],
+        -- [[Package caption Warning: Unused \\captionsetup]],
+        -- "Package typearea Warning: Bad type area settings!",
+        -- [[Package fancyhdr Warning: \\headheight is too small]],
+        -- [[Underfull \\hbox (badness [0-9]*) in paragraph at lines]],
+        -- "Package hyperref Warning: Token not allowed in a PDF string",
+        -- [[Overfull \\hbox ([0-9]*.[0-9]*pt too wide) in paragraph at lines]],
+
+        -- 'Package hyperref Warning: Option `pagebackref\'',
+        -- 'Package natbib Warning: Citation',
+        -- 'There were undefined citations',
+      }
     end,
     --<}}}
     config = function()
